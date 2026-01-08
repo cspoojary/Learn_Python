6bf888fdf1ec205301755f1831d73a8950fd622a
cspoojary <chaitanyaspoojary17@gmail.com>
2026-01-08 21:33:13 +0530

    add simple calculator

--- 30_Days_python_project/Day1.py
+++ 30_Days_python_project/Day1.py
@@ -28,25 +28,25 @@ if choice == 1:
     addition = a+b
     print("The sum of a and b = ",addition)
 
-if choice == 2:
+elif choice == 2:
     a = float(input("Enter value of a: "))
     b = float(input("Enter value of b: "))
     difference = a-b
     print("The difference of a and b is ", difference)
 
-if choice == 3:
+elif choice == 3:
     a = float(input("Enter value of a: "))
     b = float(input("Enter value of b: "))
     sum = a * b
     print("The sum of a and b is ",sum)
 
-if choice == 4:
+elif choice == 4:
     a = float(input("Enter value of a: "))
     b = float(input("Enter value of b: "))
     reminder = a/b
     print("The product of a and b is ", reminder)
 
-if choice == 5:
+elif choice == 5:
     print("Simple calculator ends here!.")
 else:
     print("Invalid input!")
