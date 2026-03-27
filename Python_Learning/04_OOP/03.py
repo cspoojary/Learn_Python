b449c3b6d25684e81518b984aca029fefe0c6660
cspoojary <chaitanyaspoojary17@gmail.com>
2026-03-27 22:08:25 +0530

    adding new file

---
+++ Python_Learning/04_OOP/03.py
@@ -0,0 +1,6 @@
+# Polymorphism
+'''
+The word comes from Greek: Poly means "many," and morph means "forms."
+
+In programming, Polymorphism is the ability of different objects to respond to the exact same method call in their own unique way. It allows you to use a single interface or function to interact with different types of objects without needing to know exactly what class they belong to.
+'''
