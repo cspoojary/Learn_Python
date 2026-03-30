8fa4e6eb71afdb73228eeb4c0adf2eb745478107
cspoojary <chaitanyaspoojary17@gmail.com>
2026-03-30 22:46:16 +0530

    adding new file

---
+++ Python_Learning/04_OOP/qusn.py
@@ -0,0 +1,125 @@
+'''
+Create a Student Class
+
+Create a class Student with attributes:
+    name
+    age
+    marks
+
+Add a method to display student details.
+'''
+
+
+
+
+'''
+Bank Account Class
+
+Create a class BankAccount with:
+    account_number
+    balance
+
+Methods:
+    deposit(amount)
+    withdraw(amount)
+    check_balance()
+'''
+
+
+'''
+Car Class
+
+Create a class Car with attributes:
+    brand
+    model
+    year
+
+Method:
+    display_info()
+'''
+
+'''
+Employee Salary Calculation
+
+Create a class Employee with:
+    name
+    salary
+
+Method:
+    calculate_bonus() (10% of salary)
+'''
+
+'''
+Inheritance Example
+
+Create a base class Animal with method:
+    sound()
+
+Create derived classes:
+    Dog
+    Cat
+
+Each class should print its own sound.
+'''
+
+'''
+Library Management Class
+
+Create a class Library with:
+    book_name
+    author
+
+Methods:
+    add_book()
+    display_books()
+'''
+
+'''
+Rectangle Area and Perimeter
+
+Create a class Rectangle with:
+    length
+    width
+
+Methods:
+    area()
+    perimeter()
+'''
+
+
+'''
+Calculator Class
+
+Create a class Calculator with methods:
+    add()
+    subtract()
+    multiply()
+    divide()
+'''
+
+'''
+Student Result System
+
+Create a class StudentResult with:
+    name
+    marks in 3 subjects
+
+Methods:
+    total_marks()
+    average_marks()
+    grade()
+'''
+
+'''
+Shopping Cart Class
+
+Create a class ShoppingCart with:
+    product name
+    price
+    quantity
+
+Methods:
+    add_product()
+    remove_product()
+    total_price()
+'''
